cbff99dc84275fafbef2f8ab3d53c148a9d51aae
misbah-aslam16 <misbahaslam000@gmail.com>
2024-08-23 23:54:58 +0500

    new

---
+++ index.js
@@ -0,0 +1,586 @@
+var shoeProduct = {
+    Nike: {
+        AirMax: {
+            img: "./images/Nike AirMax.jpg",
+            productName: "Air Max",
+            color: "Biege",
+            price: "$150",
+        },
+        AirForce1: {
+            img: "./images/AirForce1.jpg",
+            productName: "Air Force 1",
+            color: "Grey",
+            price: "$120",
+        },
+        Dunk: {
+            img: "./images/Nike Dunk.jpg",
+            productName: "Dunk",
+            color: "Blue",
+            price: "$130",
+        },
+        ZoomX: {
+            img: "./images/Nike Zoomx.jpg",
+            productName: "ZoomX",
+            color: "Blue",
+            price: "$180",
+        },
+        React: {
+            img: "./images/Nike React.jpg",
+            productName: "React",
+            color: "White",
+            price: "$140",
+        }
+    },
+    Adidas: {
+        UltraBoost: {
+            img: "./images/A Ultraboost.jpg",
+            productName: "UltraBoost",
+            color: "",
+            price: "$180",
+        },
+        StanSmith: {
+            img: "./images/A Stansmith.jpg",
+            productName: "Stan Smith",
+            color: "White",
+            price: "$100",
+        },
+        NMD: {
+            img: "./images/A Nmd.jpg",
+            productName: "NMD",
+            color: "Gray",
+            price: "$150",
+        },
+        YeezyBoost: {
+            img: "./images/A Yeezyboost.jpg",
+            productName: "Yeezy Boost",
+            color: "Beige",
+            price: "$220",
+        },
+        Gazelle: {
+            img: "./images/A Gazelle.jpg",
+            productName: "Gazelle",
+            color: "Blue",
+            price: "$90",
+        }
+    },
+    Puma: {
+        RSX: {
+            img: "./images/P Rsx.jpg",
+            productName: "RSX",
+            color: "Purple",
+            price: "$130",
+        },
+        Suede: {
+            img: "./images/P Suede.jpg",
+            productName: "Suede",
+            color: "Black",
+            price: "$110",
+        },
+        Clyde: {
+            img: "./images/P Clyde.jpg",
+            productName: "Clyde",
+            color: "White",
+            price: "$120",
+        },
+        FutureRider: {
+            img: "./images/P Futurerider.jpg",
+            productName: "Future Rider",
+            color: "Red",
+            price: "$140",
+        },
+        Disc: {
+            img: "./images/P Disc.jpg",
+            productName: "Disc",
+            color: "Gray",
+            price: "$160",
+        }
+    },
+
+    Reebok: {
+        ClassicLeather: {
+            img: "./images/Reebok Classic Leather.jpg",
+            productName: "Classic Leather",
+            color: "White",
+            price: "$85",
+        },
+        NanoX1: {
+            img: "./images/R NanoX1.jpg",
+            productName: "Nano X1",
+            color: "Black",
+            price: "$120",
+        },
+        ZigKinetica: {
+            img: "./images/R Zig Kinetica.jpg",
+            productName: "Zig Kinetica",
+            color: "Red",
+            price: "$140",
+        },
+        ClubC: {
+            img: "./images/R Clubc.jpg",
+            productName: "Club C",
+            color: "Beige",
+            price: "$90",
+        },
+        Instapump: {
+            img: "./images/R Instapump.jpg",
+            productName: "Instapump",
+            color: "Gray",
+            price: "$150",
+        }
+    },
+    Asics: {
+        GelKayano: {
+            img: "./images/As Gelkayano.jpg",
+            productName: "Gel Kayano",
+            color: "Black",
+            price: "$160",
+        },
+        GelNimbus: {
+            img: "./images/As Gelnimbus.jpg",
+            productName: "Gel Nimbus",
+            color: "Blue",
+            price: "$180",
+        },
+        GelCumulus: {
+            img: "./images/As Gelcumulus.jpg",
+            productName: "Gel Cumulus",
+            color: "Gray",
+            price: "$140",
+        },
+        GT2000: {
+            img: "./images/As Gt2000.jpg",
+            productName: "GT-2000",
+            color: "White",
+            price: "$150",
+        },
+        KayanoLite: {
+            img: "./images/As Kayanolite.jpg",
+            productName: "Kayano Lite",
+            color: "Red",
+            price: "$130",
+        }
+    },
+    UnderArmour: {
+        HOVR: {
+            img: "./images/hovr.jpg",
+            productName: "HOVR",
+            color: "Black",
+            price: "$140",
+        },
+        CurryFlow: {
+            img: "./images/curryflow.jpg",
+            productName: "Curry Flow",
+            color: "Yellow",
+            price: "$130",
+        },
+        Charged: {
+            img: "./images/charged.jpg",
+            productName: "Charged",
+            color: "Gray",
+            price: "$110",
+        },
+        ProjectRock: {
+            img: "./images/projectrock.jpg",
+            productName: "Project Rock",
+            color: "Red",
+            price: "$150",
+        },
+        Surge: {
+            img: "./images/surge.jpg",
+            productName: "Surge",
+            color: "Blue",
+            price: "$120",
+        }
+    },
+    Vans: {
+        OldSkool: {
+            img: "./images/V oldskool.jpg",
+            productName: "Old Skool",
+            color: "Black",
+            price: "$80",
+        },
+        Authentic: {
+            img: "./images/V authentic.jpg",
+            productName: "Authentic",
+            color: "White",
+            price: "$70",
+        },
+        Sk8Hi: {
+            img: "./images/V sk8hi.jpg",
+            productName: "Sk8 Hi",
+            color: "Red",
+            price: "$90",
+        },
+        Era: {
+            img: "./images/V era.jpg",
+            productName: "Era",
+            color: "Gray",
+            price: "$75",
+        },
+        SlipOn: {
+            img: "./images/V slipon.jpg",
+            productName: "Slip-On",
+            color: "Blue",
+            price: "$70",
+        }
+    },
+
+    Clarks: {
+        DesertBoot: {
+            img: "./images/C desertboot.jpg",
+            productName: "Desert Boot",
+            color: "Beige",
+            price: "$120",
+        },
+        Wallabee: {
+            img: "./images/C wallabee.jpg",
+            productName: "Wallabee",
+            color: "Brown",
+            price: "$130",
+        },
+        Unstructured: {
+            img: "./images/C unstructured.jpg",
+            productName: "Unstructured",
+            color: "Black",
+            price: "$140",
+        },
+        NatureVibe: {
+            img: "./images/C naturevibe.jpg",
+            productName: "Nature Vibe",
+            color: "Gray",
+            price: "$125",
+        },
+        TrigenicFlex: {
+            img: "./images/C trigenicflex.jpg",
+            productName: "Trigenic Flex",
+            color: "Navy",
+            price: "$140",
+        }
+    },
+    DolceGabbana: {
+        Sneakers: {
+            img: "./images/D Sneakers.jpg",
+            productName: "Sneakers",
+            color: "White",
+            price: "$350",
+        },
+        DesertBoot: {
+            img: "./images/D desertboot.jpg",
+            productName: "Desert Boot",
+            color: "Beige",
+            price: "$400",
+        },
+        LaceUp: {
+            img: "./images/D laceup.jpg",
+            productName: "Lace-Up Shoes",
+            color: "Black",
+            price: "$370",
+        },
+        SlipOn: {
+            img: "./images/D slipon.jpg",
+            productName: "Slip-On",
+            color: "Blue",
+            price: "$340",
+        },
+        Loafer: {
+            img: "./images/D loafer.jpg",
+            productName: "Loafer",
+            color: "Brown",
+            price: "$360",
+        }
+    },
+    Burberry: {
+        CheckBoots: {
+            img: "./images/B checkboots.jpg",
+            productName: "Check Boots",
+            color: "Brown",
+            price: "$450",
+        },
+        LeatherSneaker: {
+            img: "./images/B leathersneaker.jpg",
+            productName: "Leather Sneaker",
+            color: "Black",
+            price: "$420",
+        },
+        SuedeLoafer: {
+            img: "./images/B suede loafer.jpg",
+            productName: "Suede Loafer",
+            color: "Beige",
+            price: "$410",
+        },
+        Trainer: {
+            img: "./images/B trainer.jpg",
+            productName: "Trainer",
+            color: "White",
+            price: "$430",
+        },
+        ChelseaBoot: {
+            img: "./images/B chelseaboot.jpg",
+            productName: "Chelsea Boot",
+            color: "Gray",
+            price: "$440",
+        }
+    },
+    SalvatoreFerragamo: {
+        VaraPump: {
+            img: "./images/S varapump.jpg",
+            productName: "Vara Pump",
+            color: "Black",
+            price: "$550",
+        },
+        Moccasin: {
+            img: "./images/S moccasin.jpg",
+            productName: "Moccasin",
+            color: "Brown",
+            price: "$520",
+        },
+        DerbyShoe: {
+            img: "./images/S derbyshoe.jpg",
+            productName: "Derby Shoe",
+            color: "Dark Blue",
+            price: "$580",
+        },
+        Boot: {
+            img: "./images/S boot.jpg",
+            productName: "Boot",
+            color: "Beige",
+            price: "$600",
+        },
+        Sneaker: {
+            img: "./images/S sneaker.jpg",
+            productName: "Sneaker",
+            color: "White",
+            price: "$540",
+        }
+    },
+    Prada: {
+        SportShoe: {
+            img: "./images/P sports.jpg",
+            productName: "Sport Shoe",
+            color: "Black",
+            price: "$700",
+        },
+        Loafer: {
+            img: "./images/P loafer.jpg",
+            productName: "Loafer",
+            color: "Brown",
+            price: "$650",
+        },
+        AnkleBoot: {
+            img: "./images/P ankleboot.jpg",
+            productName: "Ankle Boot",
+            color: "Gray",
+            price: "$680",
+        },
+        SlipOn: {
+            img: "./images/P slipon.jpg",
+            productName: "Slip-On",
+            color: "Beige",
+            price: "$670",
+        },
+        Derby: {
+            img: "./images/P derby.jpg",
+            productName: "Derby",
+            color: "Navy",
+            price: "$690",
+        }
+    },
+    Gucci: {
+        GGCanvasSneaker: {
+            img: "./images/G ggcanvassneaker.jpg",
+            productName: "GG Canvas Sneaker",
+            color: "Beige",
+            price: "$750",
+        },
+        HorsebitLoafer: {
+            img: "./images/G horsebitloafer.jpg",
+            productName: "Horsebit Loafer",
+            color: "Black",
+            price: "$720",
+        },
+        AceSneaker: {
+            img: "./images/G acesneaker.jpg",
+            productName: "Ace Sneaker",
+            color: "White",
+            price: "$730",
+        },
+        ChelseaBoot: {
+            img: "./images/G chelseaboot.jpg",
+            productName: "Chelsea Boot",
+            color: "Brown",
+            price: "$740",
+        },
+        Sandal: {
+            img: "./images/G sandal.jpg",
+            productName: "Sandal",
+            color: "Red",
+            price: "$680",
+        }
+    },
+    Fendi: {
+        BuggieSneaker: {
+            img: "./images/F buggiesneaker.jpg",
+            productName: "Buggie Sneaker",
+            color: "Gray",
+            price: "$800",
+        },
+        LogoBoot: {
+            img: "./images/F logoboot.jpg",
+            productName: "Logo Boot",
+            color: "Black",
+            price: "$780",
+        },
+        Moccasin: {
+            img: "./images/F moccasin.jpg",
+            productName: "Moccasin",
+            color: "Brown",
+            price: "$790",
+        },
+        Sandal: {
+            img: "./images/F sandal.jpg",
+            productName: "Sandal",
+            color: "Beige",
+            price: "$760",
+        },
+        SlipOn: {
+            img: "./images/F slipon.jpg",
+            productName: "Slip-On",
+            color: "Navy",
+            price: "$770",
+        }
+    },
+    Balenciaga: {
+        TripleS: {
+            img: "./images/Ba triples.jpg",
+            productName: "Triple S",
+            color: "White",
+            price: "$900",
+        },
+        SpeedTrainer: {
+            img: "./images/Ba speedtrainer.jpg",
+            productName: "Speed Trainer",
+            color: "Black",
+            price: "$850",
+        },
+        Track: {
+            img: "./images/Ba track.jpg",
+            productName: "Track",
+            color: "Gray",
+            price: "$880",
+        },
+        CitySling: {
+            img: "./images/Ba citysling.jpg",
+            productName: "City Sling",
+            color: "Beige",
+            price: "$860",
+        },
+        Runner: {
+            img: "./images/Ba runner.jpg",
+            productName: "Runner",
+            color: "Red",
+            price: "$870",
+        }
+    }
+};
+
+
+
+var main = document.querySelector("#main")
+  var brand = document.querySelector("#brand")
+  var country = document.querySelector("#country")
+
+  brand.innerHTML = `<option value="">Select Brand </option>`
+
+country.innerHTML = `<option value="">Select type</option>`
+
+
+
+
+
+  for(var key in shoeProduct){
+
+    brand.innerHTML += `<option value="${key}">${key}</option>`
+    for(var key1 in shoeProduct[key]){
+
+
+
+        console.log(shoeProduct[key][key1]);
+    var obj = shoeProduct[key][key1]
+
+    main.innerHTML +=` <div class="card text-center" style="width: 18rem;">
+            <img src="${obj.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${obj.productName}</h5>
+              <p class="card-text">${obj.price}</p>
+              <a href="#" class="btn btn-secondary">Order Now</a><br>
+              <a href="#" class="btn btn-danger" onclick="changedanger(event,'red')"></a>
+              <a href="#" class="btn btn-success" onclick="changedanger(event,'green')"></a>
+              <a href="#" class="btn btn-primary" onclick="changedanger(event,'blue')"></a>
+            </div>
+          </div>`
+
+
+          }
+
+
+
+  }
+
+
+  function changedanger(event, color) {
+    event.preventDefault(); // Prevent default link behavior
+    const target = event.target;
+    const card = target.closest('.card'); // Find the closest parent element with the class 'card'
+    if (card) {
+        card.style.backgroundColor = color;
+    }
+}
+  
+
+
+  function setType(){
+    country.innerHTML = ""
+    country.innerHTML =`<option value ="">select type </option>`
+for(var key in shoeProduct[brand.value]){
+  country.innerHTML +=`<option value ="${key}">${key} </option>`
+
+}
+
+  }
+
+var selectedCard = document.getElementById("selectedCard")
+  function searchProduct(){
+    // console.log(makeupProduct[brand.value][country.value]);
+
+    var userVal = shoeProduct[brand.value][country.value]
+
+
+    selectedCard.innerHTML = `<div class="card text-center" style="width: 18rem;">
+            <img src="./download.jfif" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${userVal.productName}</h5>
+              <p class="card-text">${userVal.price}</p>
+              <a href="#" class="btn btn-secondary">Order Now</a>
+              <a href="#" class="btn btn-danger" onclick="changedanger(event,'red')"></a>
+              <a href="#" class="btn btn-success" onclick="changedanger(event,'green')"></a>
+              <a href="#" class="btn btn-primary" onclick="changedanger(event,'blue')"></a>
+            </div>
+          </div>`
+             main.style.display = 'none'
+
+
+
+  }
+  function clearProduct (){
+    selectedCard.style.display ='none'
+    main.style.display= "flex"
+  }
+// const Nike = {
+//     img: "./images/Nike ZoomX.jpg",
+//     productName: "Air Max",
+//     color: "Red",
+//     price: "$150",
+// };
+
+// // Access DOM elements
+// document.write('<img src="' + Nike.img +'" alt="'+Nike.color+'">' )
